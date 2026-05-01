0e398ec63220df86d5e9bfd2841e91bd76e8afa9
gagan <gaganjio14@gmail.com>
2026-05-01 13:11:43 +0530

    feat: implement saveUser method in UserService

---
+++ src/main/java/com/gagan/usertaskmanagementsystem/service/UserService.java
@@ -0,0 +1,19 @@
+package com.gagan.usertaskmanagementsystem.service;
+
+import com.gagan.usertaskmanagementsystem.entity.User;
+import com.gagan.usertaskmanagementsystem.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+}
